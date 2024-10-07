2b7f3d06812f36bf910c3f32bef5932ca000f072
Ocenwave <Owave294@gmail.com>
2024-10-07 18:52:10 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "2347066939379"
+global.ownername = "EVIL ATTITUDE"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
-global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '2347066939379'  //creator number
+global.ownername = 'EVIL ATTITUDE' //owner name
+global.botname = 'NIGHT WIZARD' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
